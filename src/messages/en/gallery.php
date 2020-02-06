61638f324ca76e8bef739d4822d8e3c88ac7a2d9
viper_work <wiperawa@gmail.com>
2020-02-06 13:49:15 +0500

    fix

---
+++ src/messages/en/gallery.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'Send' => 'Send',
+    'Egit image' => 'Edit Image',
+    'Title' => 'Title',
+    'Description' => 'Desctiption',
+    'Gallery ID' => 'Gallery Id',
+    'Sort' => 'Sort Order',
+    'Alt attrubute' => 'Alt attribute',
+    'File Path' => 'File Path',
+    'Item ID' => 'Item Id',
+    'Is Main' => 'Main Image',
+    'Model Name' => 'Model Name',
+    'Url Alias' => 'Url Alias',
+    'Crop Image' => 'Crop Image'
+];
