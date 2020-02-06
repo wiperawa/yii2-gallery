69f837e3f3186ab4135563f1f94e0e6bc57f754b
viper_work <wiperawa@gmail.com>
2020-02-06 13:50:21 +0500

    fix

--- src/views/default/modalCrop.php
+++ src/views/default/modalCrop.php
@@ -15,7 +15,7 @@ use yii\helpers\Url;
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title"><?=Yii::t('gallery', 'Crop Image')?>'</h4>
+                <h4 class="modal-title"><?=Yii::t('gallery', 'Crop Image')?></h4>
             </div>
             <div class="modal-body crop-modal-body">
                 <img src="<?= $model->getUrl() ?>" width="100%" id="cropbox"/>
